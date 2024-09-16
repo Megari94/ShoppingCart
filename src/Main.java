eb7c97172d54bd0e34824886c5847e47f3cf776a
unknown <marianelaholsbach@gmail.com>
2024-09-16 20:44:06 -0300

    Show of Carts

--- src/Main.java
+++ src/Main.java
@@ -1,110 +1,79 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
-
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to JavaLibre");
-        Scanner scanner=new Scanner(System.in);
 
-        List<Persona> person= new ArrayList<>();
-        person.add(new Persona("John", "Smith", LocalDate.of(1994, 12, 16), "123456789", 1));
+        Persona customer = new Persona("John", "Smith", LocalDate.of(1994, 12, 16), "123456789", 1);
 
-        List<Products> products=new ArrayList<>();
-        products.add(new Products(1, 200, "Laptop", "14", "Electronics"));
-        products.add(new Products(2, 100, "New Moon", "Paper", "Library"));
-        products.add(new Products(3, 500, "Kitchen", "Glass", "Others"));
-        products.add(new Products(4, 90, "Plates", "Glass", "Others"));
-        products.add(new Products(5, 10, "The little prince", "Paper", "Library"));
-        products.add(new Products(6, 30, "Airpods", "White", "Electronics"));
+        List<Products> products = new ArrayList<>();
+
+        products.add(new Products(100, 200.0, "CPU I5", "Processor", "Electronics"));
+        products.add(new Products(101, 500.0, "CPU I7", "Processor", "Electronics"));
+        products.add(new Products(102, 150.0, "Monitor", "LED", "Electronics"));
+        products.add(new Products(103, 120.0, "Keyboard", "Mechanical", "Electronics"));
+        products.add(new Products(104, 80.0, "Mouse", "Wireless", "Electronics"));
 
 
-        System.out.println("Please enter your customer number: ");
-        for (Persona p : person) {
-            System.out.println(p.getIdNumber() + ": " + p.getName() + " " + p.getLastName());
-        }
-        int idPersona = scanner.nextInt();
-        Persona selectedPerson = person.get(0);
+        products.add(new Products(200, 20.0, "1984", "Book", "Library"));
+        products.add(new Products(201, 15.0, "Harry Potter", "Book", "Library"));
+        products.add(new Products(202, 52.0, "Java 8", "Book", "Library"));
+        products.add(new Products(203, 105.0, "Clean Code", "Book", "Library"));
+        products.add(new Products(204, 90.0, "Effective Java", "Book", "Library"));
 
-        Cart cart = new Cart(selectedPerson);
 
-        boolean keepShopping = true;
-        while (keepShopping) {
-            System.out.println("What would you like to do?");
-            System.out.println("1. Add a product");
-            System.out.println("2. Remove a product");
-            System.out.println("3. View cart");
-            System.out.println("4. Checkout");
-            int option = scanner.nextInt();
+        products.add(new Products(300, 12.4, "T-shirt", "Clothing", "Others"));
+        products.add(new Products(301, 25.0, "Mug", "Drinkware", "Others"));
+        products.add(new Products(302, 30.0, "Notebook", "Stationery", "Others"));
+        products.add(new Products(303, 18.0, "Pen", "Stationery", "Others"));
+        products.add(new Products(304, 40.0, "Hat", "Clothing", "Others"));
 
-            switch (option) {
-                case 1:
 
-                    System.out.println("Available Products:");
-                    for (Products product : products) {
-                        System.out.println(product.getId() + ": " + product.getName() + " - $" + product.getPrice());
-                    }
+        Cart electronicsCart = new Cart(customer);
+        electronicsCart.addProduct(products.get(0)); // CPU I5
+        electronicsCart.addProduct(products.get(1)); // CPU I7
+        electronicsCart.addProduct(products.get(2)); // Monitor
+        electronicsCart.addProduct(products.get(3)); // Keyboard
 
-                    System.out.println("Enter the id of the product you want to add:");
-                    int idProduct = scanner.nextInt();
+        Cart libraryCart = new Cart(customer);
+        libraryCart.addProduct(products.get(6)); // Harry Potter
+        libraryCart.addProduct(products.get(7)); // Java 8
+        libraryCart.addProduct(products.get(8)); // Clean Code
 
-                    Products selectedProduct = null;
-                    for (Products product : products) {
-                        if (product.getId() == idProduct) {
-                            selectedProduct = product;
-                            break;
-                        }
-                    }
+        Cart othersCart = new Cart(customer);
+        othersCart.addProduct(products.get(10)); // T-shirt
 
-                    if (selectedProduct != null) {
-                        cart.addProduct(selectedProduct);
-                    } else {
-                        System.out.println("Product not found");
-                    }
-                    break;
 
-                case 2:
+        System.out.println("\nLibrary type products with a price greater than 100:");
+        products.stream()
+                .filter(p -> p.getPrice() > 100 && p.getType().equals("Library"))
+                .forEach(System.out::println);//.forEach(p -> System.out.println(p));
 
-                    System.out.println("Products in your cart:");
-                    for (Products product : cart.getProducts()) {
-                        System.out.println(product.getId() + ": " + product.getName() + " - $" + product.getPrice());
-                    }
 
-                    System.out.println("Enter the id of the product you want to remove:");
-                    int idProductRemove = scanner.nextInt();
+        double totalSum = products.stream().mapToDouble(Products::getPrice).sum();
+        System.out.println("\nTotal sum of the price of all products $" + totalSum);
 
-                    Products productToRemove = null;
-                    for (Products product : cart.getProducts()) {
-                        if (product.getId() == idProductRemove) {
-                            productToRemove = product;
-                            break;
-                        }
-                    }
-                    if (productToRemove != null) {
-                        cart.removeProduct(productToRemove);
-                    } else {
-                        System.out.println("Product not found in cart");
-                    }
-                    break;
 
-                case 3:
-                    cart.showCart();
-                    break;
+        double electronicsSum = products.stream()
+                .filter(p -> p.getType().equals("Electronics"))
+                .mapToDouble(Products::getPrice)
+                .sum();
+        System.out.println("\nTotal sum of the price of electronic products $" + electronicsSum);
 
-                case 4:
-                    keepShopping = false;
-                    break;
 
-                default:
-                    System.out.println("Invalid option");
-                    break;
-            }
-        }
+        System.out.println("\nShopping cart sorted by price:");
+        List<Products> sortedCart = electronicsCart.getProducts()
+                .stream()
+                .sorted(Comparator.comparingDouble(Products::getPrice))
+                .toList();
 
-        cart.showCart();
-        System.out.println("Thank you for shopping!");
-        scanner.close();
+        sortedCart.forEach(p -> System.out.println("Cart ID: 123123 | ID: " + p.getId() +
+                " | PRODUCT TYPE: " + p.getType() + " | PRODUCT NAME: " + p.getName() +
+                " | PRICE: $" + p.getPrice() + "| NAME OF BUYER: " + customer.getName()));
     }
 }
